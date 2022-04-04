9da8a7e309a2ac10895b70b485f39d6ee1fd48bc
odas0R <g.rosado@campus.fct.unl.pt>
2022-04-04 03:38:20 +0100

    feat(v0.1): added linter, and mobx state manager

--- src/components/button-counter/index.ts
+++ src/components/button-counter/index.ts
@@ -1,17 +1,17 @@
-import { LitElement, css, html } from "lit";
-import { customElement, property } from "lit/decorators.js";
-
 import { TW } from "@utils/tailwind-mixin";
+import type { TemplateResult } from "lit";
+import { LitElement, html } from "lit";
+import { customElement, property } from "lit/decorators.js";
 
 @customElement("button-counter")
 export class Counter extends TW(LitElement) {
   @property({ type: Number })
-  private counter: number = 0;
+  private counter = 0;
 
-  render() {
+  render(): TemplateResult {
     return html`
       <p class="text-red-500">Hello, ${this.counter}</p>
-      <button class="block m-5" @click="${() => (this.counter = this.counter + 1)}">Click</button>
+      <button class="block m-5" @click="${() => (this.counter += 1)}">Click</button>
     `;
   }
 }
